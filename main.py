bf71f59f4783f93c7cd93744247c50b96cc63fe0
James-Livesey <jamesliveg@gmail.com>
2020-01-16 13:01:25 +0000

    Add CLI parser

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-import logging, streaming
+import argparse
+
+import logging
+import streaming
+
+cli = argparse.ArgumentParser(
+    prog = "lcserver",
+    description = "Server environment for coordinating media management and streaming on behalf of LiveCloud apps."
+)
+
+cli.add_argument(
+    "-i", "--input-handler",
+    help = "choose input handler for command input",
+    type = str,
+    nargs = "?",
+    choices = ["tty", "lc"], 
+    default = "tty"
+)
+
+cli.add_argument(
+    "-o", "--print-handler",
+    help = "choose print handler for log output",
+    type = str,
+    nargs = "?",
+    choices = ["tty", "lc"], 
+    default = "tty"
+)
+
+arguments = cli.parse_args()
+
+print(arguments)
